0f44121d0118be294319af1b3ce0c60fa92d4ff2
Sefi Klingel <sefiklingel@gmail.com>
2018-12-13 15:51:11 +0200

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,45 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            double count = 0;
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade <= averageGrade)
+                {
+                    count += 1;
+                }
+            }
+
+            double place = count / Students.Count * 100;
+            
+            if (place > 80)
+            {
+                return 'A';
+            }
+            else if (place > 60)
+            {
+                return 'B';
+            }
+            else if (place > 40)
+            {
+                return 'C';
+            }
+            else if  (place > 20)
+            {
+                return 'D';
+            }
+
+            return 'F';
+
+        }
     }
 }
